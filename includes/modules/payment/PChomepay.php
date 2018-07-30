d991471181c4e8cee0e26b6a6dc3bd32a7285508
Jerry <>
2018-07-30 15:26:33 +0800

    daily commit

--- includes/modules/payment/PChomepay.php
+++ includes/modules/payment/PChomepay.php
@@ -225,7 +225,7 @@ class PChomepay
             }
 
             if ($order_data->status == 'W') {
-                $comment = $pay_type_note . '<br>' . sprintf('訂單交易等待中。<br>error code : %1$s<br>message : %2$s', $order_data->status_code, OrderStatusCodeEnum::getErrMsg($order_data->status_code));
+                $comment = sprintf('訂單交易等待中。<br>error code : %1$s<br>message : %2$s', $order_data->status_code, OrderStatusCodeEnum::getErrMsg($order_data->status_code));
                 order_paid($log_id, 1, $comment);
 
                 /* 修改此次支付操作的状态为已付款 */
